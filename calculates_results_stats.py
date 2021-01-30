d189f5f0bc873738e1d9a56aac5451c0408d7c10
dominikjaeckle <dominikjaeckle@gmail.com>
2021-01-30 20:12:04 +0100

    tested for uploaded images

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -100,10 +100,24 @@ def calculates_results_stats(results_dic):
         if results_dic[key][3] == 1 and results_dic[key][2] == 1:
             results_stats_dic['n_correct_breed'] += 1
             
-    results_stats_dic['pct_match'] = (results_stats_dic['n_match'] / results_stats_dic['n_images']) * 100
-    results_stats_dic['pct_correct_dogs'] = (results_stats_dic['n_correct_dogs'] / results_stats_dic['n_dogs_img']) * 100
-    results_stats_dic['pct_correct_breed'] = (results_stats_dic['n_correct_breed'] / results_stats_dic['n_dogs_img']) * 100
+    if results_stats_dic['n_match'] > 0:
+        results_stats_dic['pct_match'] = (results_stats_dic['n_match'] / results_stats_dic['n_images']) * 100
+    else:
+        results_stats_dic['pct_match'] = 0
+
+    if results_stats_dic['n_correct_dogs'] > 0:
+        results_stats_dic['pct_correct_dogs'] = (results_stats_dic['n_correct_dogs'] / results_stats_dic['n_dogs_img']) * 100
+    else: 
+        results_stats_dic['pct_correct_dogs'] = 0
+
+    if results_stats_dic['n_correct_breed'] > 0:
+        results_stats_dic['pct_correct_breed'] = (results_stats_dic['n_correct_breed'] / results_stats_dic['n_dogs_img']) * 100
+    else: 
+        results_stats_dic['pct_correct_breed'] = 0
+        
     if results_stats_dic['n_notdogs_img'] > 0:
         results_stats_dic['pct_correct_notdogs'] = (results_stats_dic['n_correct_notdogs'] / results_stats_dic['n_notdogs_img']) * 100
+    else:
+        results_stats_dic['pct_correct_notdogs'] = 0
 
     return results_stats_dic
